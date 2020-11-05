c1783da87dd97ed7b509fec85a6c1a9825944de3
Akenaftw <felix.j.c.vanroost@gmail.com>
2020-11-05 11:15:29 +0100

    initial push of project with basic setup

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+declare(stric_type=1);
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
